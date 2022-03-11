ebcc8bde218efeb06514ef6063c3874ab89678bf
dumb-programmer <asadkhan6192@gmail.com>
2022-03-11 23:46:00 +0500

    Implement utility function getDate

---
+++ src/components/Date.js
@@ -0,0 +1,21 @@
+// courtesy  https://stackoverflow.com/questions/32378590/set-date-input-fields-max-date-to-today
+function getDate() {
+  let today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth() + 1; //January is 0!
+  let yyyy = today.getFullYear();
+
+  if (dd < 10) {
+    dd = "0" + dd;
+  }
+
+  if (mm < 10) {
+    mm = "0" + mm;
+  }
+
+  today = yyyy + "-" + mm + "-" + dd;
+
+  return today;
+}
+
+export default getDate;
