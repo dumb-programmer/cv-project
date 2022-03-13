35771e69fe923d102546153fbed24c992d89aa3c
dumb-programmer <asadkhan6192@gmail.com>
2022-03-13 14:07:37 +0500

    Add CV component and conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import React from "react";
 import Form from "./components/Form";
 import getDate from "./components/Date";
 import EducationInfo from "./components/EducationInfo";
 import Job from "./components/JobInfo";
-import React from "react";
+import CV from "./components/CV";
 
 class App extends React.Component {
   constructor(props) {
@@ -17,21 +18,34 @@ class App extends React.Component {
       educationList: [new EducationInfo()],
       jobsList: [new Job()],
       todayDate: getDate(),
+      formToggle: true,
     };
     this.setState = this.setState.bind(this);
   }
   render() {
-    return (
-      <div className="App">
-        <Form
-          generalInfo={this.state.generalInfo}
-          educationList={this.state.educationList}
-          jobsList={this.state.jobsList}
-          todayDate={this.state.todayDate}
-          setState={this.setState}
-        />
-      </div>
-    );
+    if (this.state.formToggle) {
+      return (
+        <div className="App">
+          <Form
+            generalInfo={this.state.generalInfo}
+            educationList={this.state.educationList}
+            jobsList={this.state.jobsList}
+            todayDate={this.state.todayDate}
+            setState={this.setState}
+          />
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <CV
+            generalInfo={this.state.generalInfo}
+            educationList={this.state.educationList}
+            jobsList={this.state.jobsList}
+          />
+        </div>
+      );
+    }
   }
 }
 
---
+++ src/components/CV.js
@@ -0,0 +1,64 @@
+import React from "react";
+import uniqid from "uniqid";
+import "./CV.css";
+
+class CV extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const { generalInfo, educationList, jobsList } = this.props;
+    const FileRead = new FileReader();
+    return (
+      <div className="grid-container">
+        <aside>
+          <h1>{generalInfo.name}</h1>
+          <h2>Personal Info</h2>
+          <b>
+            <p>Email</p>
+          </b>
+          <p className="small-text">{generalInfo.email}</p>
+          <b>
+            <p>Phone</p>
+          </b>
+          <p className="small-text">{generalInfo.phone}</p>
+        </aside>
+        <main>
+          <div className="main-content">
+            <div className="education-info" key={uniqid()}>
+              <h1>Education</h1>
+              {educationList.map((education) => {
+                return (
+                  <div key={uniqid()}>
+                    <h2>{education.schoolName}</h2>
+                    <p>{education.degree}</p>
+                    <p>
+                      {education.startDate} - {education.endDate}
+                    </p>
+                  </div>
+                );
+              })}
+            </div>
+
+            <div className="job-info">
+              <h1>Job Experience</h1>
+              {jobsList.map((job) => {
+                return (
+                  <div key={uniqid()}>
+                    <h2>{job.company}</h2>
+                    <p>{job.title}</p>
+                    <p>
+                      {job.startDate} - {job.endDate}
+                    </p>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default CV;
